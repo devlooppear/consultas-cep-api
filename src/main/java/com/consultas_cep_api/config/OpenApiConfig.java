ca15ae6bc2bd7e795fb66315d5e1e2f608f31596
devlooppear <iago.profissional.developer@gmail.com>
2026-05-03 18:29:20 -0300

    configuracao swagger mais simples

--- src/main/java/com/consultas_cep_api/config/OpenApiConfig.java
+++ src/main/java/com/consultas_cep_api/config/OpenApiConfig.java
@@ -1,28 +1,24 @@
 package com.consultas_cep_api.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Value;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(info = @Info(title = "Consultas CEP API", description = "Documentacao OpenAPI da API de consultas de CEP.", version = "v1", contact = @Contact(name = "consultas-cep-api")))
 public class OpenApiConfig {
 
     @Bean
-    OpenAPI consultasCepOpenAPI(
-            @Value("${spring.application.name}") String applicationName,
-            @Value("${server.servlet.context-path:/}") String contextPath) {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Consultas CEP API")
-                        .description("Documentacao OpenAPI da API de consultas de CEP.")
-                        .version("v1")
-                        .contact(new Contact().name(applicationName)))
-                .servers(List.of(new Server()
-                        .url(contextPath)));
+    ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    YAMLMapper yamlMapper() {
+        return new YAMLMapper();
     }
 }
