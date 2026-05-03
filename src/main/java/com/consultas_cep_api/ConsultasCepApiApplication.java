7382f0e1dc25d40ff64f13d4f21e157bd15b51b1
devlooppear <iago.profissional.developer@gmail.com>
2026-05-03 16:07:37 -0300

    first commit

---
+++ src/main/java/com/consultas_cep_api/ConsultasCepApiApplication.java
@@ -0,0 +1,13 @@
+package com.consultas_cep_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultasCepApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultasCepApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consultas_cep_api/ConsultasCepApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultas_cep_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultasCepApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
